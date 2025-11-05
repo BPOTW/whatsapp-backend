ac42a945dbff5b7086aa2aaddfd5e6db21d458d9
Muhammad Zain Ali <133132057+BPOTW@users.noreply.github.com>
2025-11-05 22:02:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {Server} from "socket.io";
+import http from "http";
+
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:5173",
+        method:["GET", "POST"],
+        credentials:true,
+    }
+});
+
+io.on("connection",(socket)=>{
+    console.log("Connected to websocket : ", socket.id);
+
+    socket.on('join',(roomId)=>{
+        socket.join(roomId);
+    });
+    socket.on('leave',(roomId)=>{
+        socket.leave(roomId);
+    });
+    socket.on('send',(message)=>{
+        console.log(message);
+        socket.to(message.room).emit("message",message);
+    });
+});
+
+server.listen(5050,()=>{
+    console.log('Server started successfully on port 5050');
+})
